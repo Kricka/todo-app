01a255bb30b8b337a00064828dbe73f98c19a025
Milan <kricka.milan@gmail.com>
2022-12-15 13:04:32 +0100

    Implemented a Complete and Exit button

--- gui.py
+++ gui.py
@@ -6,9 +6,11 @@ input_box=sg.InputText(tooltip="Enter a todo",key='todo')
 add_button=sg.Button('Add')
 list_box=sg.Listbox(values=functions.get_todos(),key='todos',enable_events=True,size=[45,10])
 edit_button=sg.Button('Edit')
+complete_button=sg.Button('Complete')
+exit_button=sg.Button('Exit')
 
 window=sg.Window('My-To-Do-App',
-                 layout=[[label],[input_box,add_button],[list_box,edit_button]],
+                 layout=[[label],[input_box,add_button],[list_box,edit_button],[complete_button,exit_button]],
                  font=('Helvetica',20))
 
 while True:
@@ -34,6 +36,17 @@ while True:
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+
+        case 'Complete':
+            todo_to_complete=values['todos'][0]
+            todos=functions.get_todos()
+            index=todos.index(todo_to_complete)
+            todos.pop(index)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value=None)
+        case 'Exit':
+            break
 
         case sg.WIN_CLOSED:
             break
